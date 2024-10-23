1505e5bcb595375a7b2be1feea6d7ce83812b5e6
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-10-23 16:52:09 +0900

    start

---
+++ Algorithm/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+       long c = scanner.nextLong();
+
+
+        System.out.println(a + b + c);
+
+        scanner.close();
+    }
+}
