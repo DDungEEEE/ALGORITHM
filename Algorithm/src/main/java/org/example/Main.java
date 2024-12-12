7e01c481320b0ade949ca84952f68b3cc301a72b
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-12-12 13:42:39 +0900

    update

--- Algorithm/src/main/java/org/example/Main.java
+++ Algorithm/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,15 +11,28 @@ public class Main {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int count = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer ts;
 
-        for(int i = 0; i <count; i++){
-           ts = new StringTokenizer(bufferedReader.readLine(), " ");
-           bufferedWriter.write((Integer.parseInt(ts.nextToken()) + Integer.parseInt(ts.nextToken()))+ "\n");
+        int n = Integer.parseInt(bufferedReader.readLine());
+        double[] numList = new double[n];
+        int max = 1;
+        int i=0;
+        double result=0;
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+        while (stringTokenizer.hasMoreTokens()){
+            int num = Integer.parseInt(stringTokenizer.nextToken());
+            numList[i] = num;
+            max = Math.max(max, num);
+            i++;
         }
-        bufferedReader.close();;
-        bufferedWriter.flush();;
+
+        for(int a=0; a<n; a++){
+            result += numList[a] / max * 100;
+        }
+
+        bufferedWriter.write(result/n + "");
+        bufferedReader.close();
+        bufferedWriter.flush();
         bufferedWriter.close();
 
     }
