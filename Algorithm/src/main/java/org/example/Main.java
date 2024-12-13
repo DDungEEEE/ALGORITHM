3d181b48888b99908f37a9423ef6074a524d7781
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-12-13 17:54:30 +0900

    update

--- Algorithm/src/main/java/org/example/Main.java
+++ Algorithm/src/main/java/org/example/Main.java
@@ -4,13 +4,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        System.out.println("         ,r'\"7\n" +
-                "r`-_   ,'  ,/\n" +
-                " \\. \". L_r'\n" +
-                "   `~\\/\n" +
-                "      |\n" +
-                "      |");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int count = 0;
+        int origin = num;
+
+
+
+        while (true){
+            num = func(num);
+            count++;
+            if(num == origin){
+                break;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static int func(int a){
+        if(a <10){
+            int r = a;
+            return r * 10 + r;
+        }else{
+            int r = a % 10; // 십의자리
+            int t = a/10 + r; // 일의자리
+            return r * 10 + t%10;
+        }
+
     }
 }
