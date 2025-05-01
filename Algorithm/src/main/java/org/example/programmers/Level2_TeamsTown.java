c13cc81ad9affe4a9274ad145ac195572d54612a
손원익 <102235796+DDungEEEE@users.noreply.github.com>
2025-05-01 09:05:57 +0900

    Level2: 같은 문자열 제거하기

---
+++ Algorithm/src/main/java/org/example/programmers/Level2_TeamsTown.java
@@ -0,0 +1,24 @@
+package org.example.programmers;
+
+import java.util.Stack;
+
+public class Level2_TeamsTown {
+    public static void main(String[] args) {
+        String s = "baabaa";
+        String s2 = "cdcd";
+
+        System.out.println(solution(s));
+    }
+    static int solution(String s){
+        String[] str = s.split("");
+        Stack<String> result = new Stack<>();
+        for(int i=0; i<str.length; i++){
+            if(!result.isEmpty() && result.peek().equals(str[i])){
+                result.pop();
+            }else{
+                result.push(str[i]);
+            }
+        }
+        return result.isEmpty() ? 1 : 0;
+    }
+}
