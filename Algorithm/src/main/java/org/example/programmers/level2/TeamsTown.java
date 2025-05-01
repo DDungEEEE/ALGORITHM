381211de23225da08074fc53261371b67e2c0ddc
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-05-01 11:28:32 +0900

    백준 Level2: 주식 가격 생존기간 구하기

--- Algorithm/src/main/java/org/example/programmers/level2/TeamsTown.java
+++ Algorithm/src/main/java/org/example/programmers/level2/TeamsTown.java
@@ -1,8 +1,8 @@
-package org.example.programmers;
+package org.example.programmers.level2;
 
 import java.util.Stack;
 
-public class Level2_TeamsTown {
+public class TeamsTown {
     public static void main(String[] args) {
         String s = "baabaa";
         String s2 = "cdcd";
