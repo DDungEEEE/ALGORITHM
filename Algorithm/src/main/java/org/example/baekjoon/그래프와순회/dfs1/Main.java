05b114ad30a55ff9de846bd3cb6033799ebe4d13
DDungEEEE <thsdnjsdlr12@gmail.com>
2025-10-22 08:44:31 +0900

    bfs 연습

---
+++ Algorithm/src/main/java/org/example/baekjoon/그래프와순회/dfs1/Main.java
@@ -0,0 +1,68 @@
+package org.example.baekjoon.그래프와순회.dfs1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] order;
+    static int count = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 정점 수
+        int m = Integer.parseInt(st.nextToken()); // 간선 수
+        int r = Integer.parseInt(st.nextToken()); // 시작 정점
+
+        graph = new ArrayList[n + 1];
+        for(int i = 1; i <= n; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        // 번호가 작은 정점부터 방문하도록 정렬
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        visited = new boolean[n + 1];
+        order = new int[n + 1];
+
+        dfs(r);
+
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(order[i]).append("\n");
+        }
+        System.out.print(sb);
+
+    }
+
+    static void dfs(int node){
+        visited[node] = true;
+        order[node] = count++;
+
+        for(int next : graph[node]){
+            if(!visited[next]){
+                dfs(next);
+            }
+        }
+    }
+}
